660987e62dd89d47e93b171426b68164453398d0
Borrison <borissoto@gmail.com>
2021-03-23 00:59:40 -0400

    building post setup and handling CORS erros

--- comments/index.js
+++ comments/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const bodyParser = requrie('body-parser');
+const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/commnets', (req, res) => {
+app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
